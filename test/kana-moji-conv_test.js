8a249a4a1d548cc5d6c53836093d40e427beee40
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2017-03-27 02:05:21 +0900

    add tests for toKataTable and toHiraTable to see if "vu" is correctly registered.

--- test/kana-moji-conv_test.js
+++ test/kana-moji-conv_test.js
@@ -6,6 +6,8 @@ const KanaMojiConv = require("../index").KanaMojiConv;
 describe("KanaMojiConv", () => {
   const hiraSmallA = String.fromCharCode(0x3041);
   const kataSmallA = String.fromCharCode(0x30a1);
+  const hiraVu = String.fromCharCode(0x3094);
+  const kataVu = String.fromCharCode(0x30f4);
 
   it("canary test", () =>  {
     expect(KanaMojiConv).to.be.an("function");
@@ -33,12 +35,22 @@ describe("KanaMojiConv", () => {
       const table = KanaMojiConv.toKataTable;
       expect(table[hiraSmallA]).to.deep.equal(kataSmallA);
     });
+
+    it("expects to return kataVu when hiraVu is given", () => {
+      const table = KanaMojiConv.toKataTable;
+      expect(table[hiraVu]).to.deep.equal(kataVu);
+    });
   });
 
   describe("toKataTable", () => {
     it("expects to return hiraSmallA when kataSmallA is given", () => {
       const table = KanaMojiConv.toHiraTable;
       expect(table[kataSmallA]).to.deep.equal(hiraSmallA);
+    });
+
+    it("expects to return hiraVu when kataVu is given", () => {
+      const table = KanaMojiConv.toHiraTable;
+      expect(table[kataVu]).to.deep.equal(hiraVu);
     });
   });
 });
