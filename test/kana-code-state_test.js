b2b3278f62eb35befc267a382a31b9cc85967c80
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2017-04-02 00:45:23 +0900

    refactoring of tests for KanaCodeState

--- test/kana-code-state_test.js
+++ test/kana-code-state_test.js
@@ -6,22 +6,20 @@ const KanaCodeState = require("../index").KanaCodeState;
 describe("KanaCodeState", () => {
   context("when roma-kana-table.json is given", () => {
     const jsonData = require("../lib/roma-kana-table.json");
+    let codeState;
 
     describe("setupCodeTable", () => {
-      it("expects to setup codeState.fixTable", () => {
-        const codeState = new KanaCodeState;
-
+      beforeEach(() => {
+        codeState = new KanaCodeState;
         codeState.setupCodeTable(jsonData);
+      });
 
+      it("expects to setup codeState.fixTable", () => {
         expect(codeState).to.be.an.instanceof(KanaCodeState);
         expect(codeState.fixTable).to.not.be.empty;
       });
 
       it("expects to setup codeState.unfixTable", () => {
-        const codeState = new KanaCodeState;
-
-        codeState.setupCodeTable(jsonData);
-
         expect(codeState).to.be.an.instanceof(KanaCodeState);
         expect(codeState.unfixTable).to.not.be.empty;
       });
